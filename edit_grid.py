87ced8ba8beb3a3df086cc704a2bfa101d97ab1a
KaziJaber <jaforjaber@gmail.com>
2020-12-23 16:33:53 +0600

    updated + minor fixes | 4:34 PM 12/23/2020

--- edit_grid.py
+++ edit_grid.py
@@ -6,7 +6,7 @@ print(_USER_DATA.dota2_account_id)
 
 
 favorite_heroes = [1]
-all_heroes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 119, 120, 121, 126, 128, 129]
+all_heroes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 119, 120, 121, 123, 126, 128, 129]
 def create_hero_grid(grid_name,sort_by):
   with open(_USER_DATA.dota2_account_id + "/hero_grid_config.json") as json_file,open('heroes_data.csv','r') as hero_json:
       pos = False
@@ -31,7 +31,7 @@ def create_hero_grid(grid_name,sort_by):
       for x in range(0,1100,63):
         for y in range(0,510,83):
           it+=1
-          if it>118:
+          if it>119:
             break
           df = df.sort_values(sort_by,ascending=False)
           now_hero = df.iloc[it]
@@ -69,7 +69,7 @@ def role_grid(grid_name,heroes):
 
       data['configs'][pos] = {'config_name':grid_name,'categories':[]}
       for x in range(0,1100,int(1100)):
-        for y in range(0,510,int(510/6)-1):
+        for y in range(0,510,int(510/len(roles))-1):
           it+=1
           if it+1>len(roles):
             break
@@ -79,4 +79,3 @@ def role_grid(grid_name,heroes):
     
 role_grid('Hero_roles',all_heroes)
 create_hero_grid('best_heroes','winrate')
-create_hero_grid('base_attack','base_attack_max')
